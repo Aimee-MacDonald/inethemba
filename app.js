4006c96f020997287a9e2dbc21e9239b3ff7bf3f
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-01-29 22:46:28 +0200

    Initial Layout

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.status(200).sendFile(__dirname + "/public/views/index.html");
+});
+
 app.listen(8080, console.log("Server Listening on Port: 8080"));
